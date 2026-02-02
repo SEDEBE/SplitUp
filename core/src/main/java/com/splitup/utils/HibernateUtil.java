4ea34990cea9020f5f2a6e7b0c0b7ec5cc2c5b27
Alejandro Córdoba Pérez <alejandrocordoba28@gmail.com>
2026-02-02 21:46:11 +0100

    Update HibernateUtil

--- core/src/main/java/com/splitup/utils/HibernateUtil.java
+++ core/src/main/java/com/splitup/utils/HibernateUtil.java
@@ -3,20 +3,33 @@ package com.splitup.utils;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
 
     static {
         try {
+            Properties props = new Properties();
+            try (InputStream in = HibernateUtil.class.getClassLoader().getResourceAsStream("hibernate.properties")) {
+                if (in == null) {
+                    throw new IllegalStateException("No se encontró hibernate.properties en src/main/resources");
+                }
+                props.load(in);
+            }
+
             sessionFactory = new Configuration()
+                    .addProperties(props)
                     .addAnnotatedClass(com.splitup.model.User.class)
                     .addAnnotatedClass(com.splitup.model.ExpenseGroup.class)
                     .addAnnotatedClass(com.splitup.model.GroupMember.class)
-
                     .buildSessionFactory();
+
         } catch (Throwable ex) {
-            System.err.println(" Error al crear SessionFactory");
+            System.err.println("Error al crear SessionFactory");
             throw new ExceptionInInitializerError(ex);
         }
     }
